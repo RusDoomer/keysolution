e55c0cda789807df9f3a727e7fd46ff56e505f0b
RusDoomer <126584145+RusDoomer@users.noreply.github.com>
2023-12-13 03:44:18 -0500

    Update classify.mjs

--- js/classify.mjs
+++ js/classify.mjs
@@ -168,7 +168,7 @@ function trigrams(key) {
 
     if (
         new Set(key.map(x => hand(x))).size == 1 &&
-        row(key[0]) == row(key[1]) && row(key[1]) == row(key[2]) &&
+        new Set(key.map(x => row(x))).size == 1 &&
         ordered(key)
     ) {
         buckets.push('ONE')
